d189b75578c17622f8c7f1c51f98c56fa846b350
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-04-24 02:51:37 +0530

    refactor: remove authentication, device, and fingerprint modules and associated routes

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,26 +2,10 @@
 
 from app.models.robot import (
     Robot,
-    Device,
-    DeviceFingerprint,
-    AuthenticationSession,
-    LoginAttempt,
     RobotStatus,
-    DeviceType,
-    AuthenticationStatus,
-    OtpPurpose,
-    PasswordChangeReason,
 )
 
 __all__ = [
     "Robot",
-    "Device",
-    "DeviceFingerprint",
-    "AuthenticationSession",
-    "LoginAttempt",
     "RobotStatus",
-    "DeviceType",
-    "AuthenticationStatus",
-    "OtpPurpose",
-    "PasswordChangeReason",
 ]
