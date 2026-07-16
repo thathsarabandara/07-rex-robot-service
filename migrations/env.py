e469de879ad026651a8b1d2fabb65ec7d6c3e5e4
Thathsara Bandara <thathsaraarumapperuma@gmail.com>
2026-07-16 19:29:45 +0530

    refactor: move imports to the top of migrations/env.py for consistency

--- migrations/env.py
+++ migrations/env.py
@@ -1,10 +1,9 @@
+import os
+import sys
 from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
-
-import os
-import sys
 
 # Add the project root directory to the Python path
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
